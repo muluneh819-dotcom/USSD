3ff6882b067bd7798c2952220021746250a96a63
muluneh23 <muluneh819@gmail.com>
2025-10-15 06:00:20 -0700

    upadate main.py

--- main.py
+++ main.py
@@ -12,22 +12,47 @@ while True:
     # -------------------------------
     # Group 1: Check Balance
     if option == "1":
-        pass   # <-- Group 1: Insert your code here
+        print(f"Your current balance is: {balance} birr.")
 
     # -------------------------------
     # Group 2: Buy Data
     elif option == "2":
         
-        pass   # <-- Group 2: Insert your code here
-
+        print("Data packages:")
+        print("1. 200mb=25 birr.")
+        print("2. 500mb=50 birr.")
+        print("3. 1GB=100 birr.")
+        choice = input("choose a data package(1-3):")
+        if choice == "1" and balance >=25:
+            balance -= 25
+            print("you bought 200mb of data")
+        elif choice == "2" and balance >=50:
+            balance-=50
+            print("you bought 500mb of data ") 
+        elif choice == "3" and balance >=100:
+            balance -= 100
+            print("you bought 1GB of data")
+        else:
+            print("insufficient balance ") 
+        print(f"remaining balance:{balance} birr.")
     # -------------------------------
     # Group 3: Buy Airtime
     elif option == "3":
-        pass   # <-- Group 3: Insert your code here
+        try:
+            amount = float(input("Enter airtime amount:"))
+            if amount <= balance:
+                balance -= amount
+                print(f"you bought {amount} birr airtime.") 
+                print(f"Remaining balance: {balance} birr")
+            else:
+                print("insufficient balance")    # <-- Group 3: Insert your code here
 
     # -------------------------------
     # Group 4: Exit & Invalid Input
     elif option == "4":
-        pass   # <-- Group 4: Insert your code to handle exit here
+        print("Thank you for using our service. Goodbye!")
+        break
+   # <-- Group 4: Insert your code to handle exit here
     else:
-        pass   # <-- Group 4: Insert your code to handle invalid input here
+        print("Invalid option. Please try again.")
+  # <-- Group 4: Insert your code to handle invalid input here
